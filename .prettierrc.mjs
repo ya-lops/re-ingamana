ae44800a1d95218b6ebf784fe2c8429b915c429c
Vitaliy L <lops@list.ru>
2026-03-01 21:28:32 +0300

    init

---
+++ .prettierrc.mjs
@@ -0,0 +1,19 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+    {
+      files: '*.svg',
+      options: {
+        parser: 'html', // Используем HTML парсер для SVG
+      },
+    },
+  ],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {},
+  build: {
+    inlineStylesheets: "always",
+  },
+  vite: {
+    build: {
+      cssCodeSplit: true,
+    },
+    css: {
+      devSourcemap: true,
+    },
+  },
+  devToolbar: {
+    enabled: false,
+  },
+});
